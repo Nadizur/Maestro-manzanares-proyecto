1e70b1f03f42bee05218ba2edb3e8137faa88561
Nadizur <danielrz9814@outlook.com>
2024-06-01 01:53:31 -0600

    Revert "Se agrego el proyecto"
    This reverts commit 6c0eb3c93d2e96751e3ef097ba40d106e55746f2.

--- app/src/main/java/com/example/proyectomanzanares/data/model/OrderDetails.kt
+++
@@ -1,9 +0,0 @@
-package com.example.proyectomanzanares.data.model
-
-class OrderDetails (
-    val customerId: Int,
-    val orderDate: String,
-    val productId: Int,
-    val quantity: Int,
-    val unitPrice: Double
-)
